d2b8aab2bfbfd8fa44c87f9c26b66e12666317df
Shahzaib <shahzaibhassan260@gmail.com>
2021-09-09 12:24:38 +0500

    Weather-App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+const api = {
+    key = "99daecf0a2d2c166467c34d222160f57 ",
+    base = "http://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+    return ( <
+        div className = "App" >
+        <main>
+        <
+        div className = "search-box" >
+        <
+        input type = "text"
+        className = "search-bar"
+        placeholder = "Search...." / >
+        <
+        /div> < /
+        main > <
+        /div>
+    );
+}
+
+export default App;
